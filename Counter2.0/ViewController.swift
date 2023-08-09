191728ad8fb2298fac78425b7fdb495891fd20da
o-shea <atmos.o.shea@gmail.com>
2023-08-09 21:46:53 +0400

    Initial Commit

---
+++ Counter2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter2.0
+//
+//  Created by MAC on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Counter2.0UITests/Counter2_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Counter2_0UITestsLaunchTests.swift
+//  Counter2.0UITests
+//
+//  Created by MAC on 09.08.2023.
+//
+
+import XCTest
+
+final class Counter2_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
